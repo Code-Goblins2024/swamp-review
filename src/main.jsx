c0d883a79300e38e1b7e2b72f65b76bfdc709abf
Jordan Sheehan <jordansheehan@ufl.edu>
2024-09-27 19:00:09 -0400

    Add CssBaseline to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import { CssBaseline } from "@mui/joy";
 import "./index.css";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
@@ -10,6 +11,7 @@ import "@fontsource/inter";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
+		<CssBaseline />
 		<App />
 	</StrictMode>
 );
