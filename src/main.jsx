3bbb00784bd6f810c8b89c1e10fa42422b5bc795
Jordan Sheehan <jordansheehan@ufl.edu>
2024-09-27 17:29:24 -0400

    Install mui material and joy

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "@fontsource/inter";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
