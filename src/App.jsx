075c51c0b930d4490ba6f42d0beb9dd583618a57
Jordan Sheehan <jordansheehan@ufl.edu>
2024-09-28 17:58:32 -0400

    Add auth getter and listener to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,38 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import SignInUp from "./pages/SignInUp";
+import { useState, useEffect } from "react";
+import useAuth from "./store/authStore";
+import supabase from "./config/supabaseClient";
+
 const App = () => {
+	const { setSession } = useAuth();
+	const [loading, setLoading] = useState(true);
+
+	// All logic for loading the application
+	const loadApp = async () => {
+		const { data, error } = await supabase.auth.getSession();
+		if (!error) setSession(data.session);
+
+		setLoading(false);
+	};
+
+	useEffect(() => {
+		loadApp();
+	}, []);
+
+	// Auth state change listener
+	useEffect(() => {
+		const {
+			data: { subscription },
+		} = supabase.auth.onAuthStateChange((_event, session) => {
+			setSession(session);
+		});
+
+		return () => subscription.unsubscribe();
+	}, []);
+
+	if (loading) return null;
+
 	return (
 		<>
 			<p className="read-the-docs">Vite is running.</p>
