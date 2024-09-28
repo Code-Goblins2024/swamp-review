4fc8262749cc419e959f30e954655aed5feedfec
Jordan Sheehan <jordansheehan@ufl.edu>
2024-09-28 14:30:10 -0400

    Sign in/up form data handling and validation

---
+++ src/pages/FormItem.jsx
@@ -0,0 +1,35 @@
+import { FormControl, FormLabel, Input, Typography } from "@mui/joy";
+import PropTypes from "prop-types";
+
+const FormItem = ({ label, name, type, value, error, onChange }) => {
+	return (
+		<FormControl sx={{ width: "100%" }}>
+			<FormLabel>{label}</FormLabel>
+			<Input
+				value={value}
+				onChange={onChange}
+				error={!!error}
+				type={type}
+				name={name}
+				size="sm"
+				sx={{ fontWeight: 500 }}
+			/>
+			{error && (
+				<Typography sx={{ marginTop: "2px", fontWeight: 400 }} level="body-xs" color="danger">
+					Error: {error}
+				</Typography>
+			)}
+		</FormControl>
+	);
+};
+
+FormItem.propTypes = {
+	label: PropTypes.string.isRequired,
+	name: PropTypes.string.isRequired,
+	type: PropTypes.string.isRequired,
+	value: PropTypes.string.isRequired,
+	error: PropTypes.string,
+	onChange: PropTypes.func.isRequired,
+};
+
+export default FormItem;
--- src/pages/SignInUp.jsx
+++ src/pages/SignInUp.jsx
@@ -1,14 +1,59 @@
-import { Typography } from "@mui/joy";
-import Box from "@mui/joy/Box";
-import Stack from "@mui/joy/Stack";
-import FormControl from "@mui/joy/FormControl";
-import FormLabel from "@mui/joy/FormLabel";
-import Input from "@mui/joy/Input";
-import Button from "@mui/joy/Button";
-import Divider from "@mui/joy/Divider";
+import { Typography, Box, Stack, Button, Divider } from "@mui/joy";
+import { useState } from "react";
+import FormItem from "./FormItem";
 
 const SignInUp = () => {
-	const handleSubmit = () => {};
+	const uflEmailPattern = /^[a-zA-Z0-9._%+-]+@([a-zA-Z0-9.-]+\.)*ufl\.edu/;
+	const formDataTemplate = {
+		email: "",
+		password: "",
+		confirmPassword: "",
+	};
+
+	const [signInActive, setSignInActive] = useState(true);
+	const [formData, setFormData] = useState({ ...formDataTemplate });
+	const [formErrors, setFormErrors] = useState({ ...formDataTemplate });
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		if (!signInActive) {
+			if (!validateSignUp()) return;
+		}
+	};
+
+	const validateSignUp = () => {
+		let newFormErrors = { ...formDataTemplate };
+		if (!uflEmailPattern.test(formData.email))
+			newFormErrors = { ...newFormErrors, email: "Email must be a valid UFL email." };
+
+		if (!formData.password) newFormErrors = { ...newFormErrors, password: "Password cannot be empty" };
+
+		if (formData.password !== formData.confirmPassword)
+			newFormErrors = { ...newFormErrors, confirmPassword: "Passwords must match." };
+
+		setFormErrors(newFormErrors);
+		return Object.values(newFormErrors).every((value) => value === "");
+	};
+
+	const switchActiveForm = () => {
+		resetForm();
+		setSignInActive(!signInActive);
+	};
+
+	const resetForm = () => {
+		setFormData({ ...formDataTemplate });
+		setFormErrors({ ...formDataTemplate });
+	};
+
+	const handleFormChange = (e) => {
+		const { name, value } = e.target;
+		setFormErrors({ ...formErrors, [name]: "" });
+		setFormData({
+			...formData,
+			[name]: value,
+		});
+	};
 
 	return (
 		<Box
@@ -20,6 +65,7 @@ const SignInUp = () => {
 				alignItems: "center",
 			}}
 		>
+			{/* Sign In/Up Form Container */}
 			<Box
 				sx={{
 					width: { xs: "95%", md: "50%", lg: "40%", xl: "25%" },
@@ -36,35 +82,66 @@ const SignInUp = () => {
 						alignItems: "flex-start",
 					}}
 				>
+					{/* Header */}
 					<Typography level="h3" fontWeight="xl">
-						Sign Up
+						{signInActive ? "Sign In" : "Sign Up"}
 					</Typography>
-					<form onSubmit={handleSubmit} style={{ width: "100%" }}>
+
+					{/* Sign In/Up Form */}
+					<form onSubmit={handleSubmit} noValidate style={{ width: "100%" }}>
 						<Stack
 							direction="column"
-							spacing={2}
+							spacing={1.5}
 							sx={{
 								justifyContent: "center",
 								alignItems: "flex-start",
 							}}
 						>
-							<FormControl sx={{ width: "100%" }} required>
-								<FormLabel>Email</FormLabel>
-								<Input type="email" name="email" />
-							</FormControl>
-							<FormControl sx={{ width: "100%" }} required>
-								<FormLabel>Password</FormLabel>
-								<Input type="password" name="password" />
-							</FormControl>
-							<Button color="primary" fullWidth>
-								Sign In
-							</Button>
+							{/* Email Form Group */}
+							<FormItem
+								label="Email"
+								name="email"
+								type="email"
+								value={formData.email}
+								onChange={handleFormChange}
+								error={formErrors.email}
+							/>
+
+							{/* Password Form Group */}
+							<FormItem
+								label="Password"
+								name="password"
+								type="password"
+								value={formData.password}
+								onChange={handleFormChange}
+								error={formErrors.password}
+							/>
+
+							{/* Confirm Password Group */}
+							{!signInActive && (
+								<FormItem
+									label="Confirm Password"
+									name="confirmPassword"
+									type="password"
+									value={formData.confirmPassword}
+									onChange={handleFormChange}
+									error={formErrors.confirmPassword}
+								/>
+							)}
 						</Stack>
+
+						{/* Sign In/Sign Up Button */}
+						<Button sx={{ marginTop: "1.5rem" }} color="primary" type="submit" fullWidth>
+							{signInActive ? "Sign In" : "Sign Up"}
+						</Button>
 					</form>
+
 					<Divider />
+
+					{/* Sign up here/Sign in here message */}
 					<Box sx={{ width: "100%", display: "flex", justifyContent: "center" }}>
 						<Typography level="body-sm" fontWeight="md">
-							{"Don't have an account? "}
+							{signInActive ? "Don't have an account? " : "Already have an account? "}
 							<Typography
 								sx={{
 									"&:hover": {
@@ -73,8 +150,9 @@ const SignInUp = () => {
 								}}
 								fontWeight="lg"
 								color="primary"
+								onClick={switchActiveForm}
 							>
-								Sign up here.
+								{signInActive ? "Sign up here" : "Sign in here"}
 							</Typography>
 						</Typography>
 					</Box>
