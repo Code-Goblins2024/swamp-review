1a8bd032a2a5cfef7ad1e082a23e5ca64fd83236
erobx <erob7856@gmail.com>
2024-10-06 19:09:55 -0400

    feat: favorites, reviews, tags, ips

--- src/functions/housingQueries.js
+++ src/functions/housingQueries.js
@@ -1,7 +1,15 @@
 import supabase from "../config/supabaseClient";
 
 export const getAllHousing = async () => {
-  const { data, error } = await supabase.from("housing").select();
+  const { data, error } = await supabase.from("housing").select(`
+    name,
+    averageRating (
+      categories (
+        name
+      ),
+      averageRating
+    )
+  `);
   if (error) {
     console.log("Error retrieving housing:", error)
   }
@@ -16,24 +24,53 @@ export const getHousing = async (id) => {
   return data;
 }
 
-export const getInterestPoints = async () => {
+export const getInterestPoints = async (id) => {
   const { data, error } = await supabase.from("housing").select(
     `id,
     name,
-    interestPoints (name)
-  `)
+    interestPoints (
+      name,
+      address
+    )
+  `).eq("id", id)
   if (error) {
     console.log("Error retrieving pois:", error)
   }
   return data;
 }
 
-export const testTags = async () => {
+export const getReviewTags = async () => {
   const { data, error } = await supabase.from("reviews").select(`
-    "created_at",
+    created_at,
+    content,
     tags (
       name
     )
   `)
+  return data;
+}
+
+export const getHousingReviews = async (id) => {
+  const { data, error } = await supabase.from("housing").select(`
+    id,
+    name,
+    address,
+    reviews (
+      rating,
+      content
+    )
+  `).eq("id", id)
+  return data;
+}
+
+export const getUserFavorites = async (id) => {
+  const { data, error } = await supabase.from("users").select(`
+    housing (
+      name,
+      averageRating (
+        averageRating
+      )
+    )
+  `).eq("id", id)
   return data;
 }
