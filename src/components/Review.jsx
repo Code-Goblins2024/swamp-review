58be32ce6a508835609c4f5b5a4ea6b968357d36
Jordan Sheehan <jordansheehan26@gmail.com>
2024-10-28 18:49:22 -0400

    Update Review component to receive the review data as a prop

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,64 +1,56 @@
 import { Card, Stack, Typography, Box } from "@mui/joy";
 import { Grid2 } from "@mui/material";
 import Rating from "./Rating";
+import PropTypes from "prop-types";
 
-const Review = () => {
-	const tempCategories = [
-		{
-			name: "Location",
-			rating: 4.0,
-		},
-		{
-			name: "Rooms",
-			rating: 3.9,
-		},
-		{
-			name: "Bathrooms",
-			rating: 2.4,
-		},
-		{
-			name: "Amenities",
-			rating: 5.0,
-		},
-	];
-
+const Review = ({ review }) => {
 	return (
 		<Card>
 			<Stack spacing={2} sx={{ padding: "0.5rem" }}>
 				<Grid2 container spacing={2} sx={{ flexGrow: 1 }}>
-					{tempCategories.map((category) => (
-						<Grid2 key={category.name} size={{ xs: 6, md: 3 }}>
-							<Rating type="review" title={category.name} rating={category.rating} />
+					{review.ratings.map((rating, index) => (
+						<Grid2 key={index} size={{ xs: 6, md: 3 }}>
+							<Rating type="review" title={rating.category.name} rating={rating.value} />
 						</Grid2>
 					))}
 				</Grid2>
-				<Typography level="body-md">
-					Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit corporis commodi illum reprehenderit
-					aperiam neque, culpa fugit sint numquam vitae dolorum natus quas, fuga officiis ut eaque ullam
-					similique nostrum?
-				</Typography>
+				<Typography level="body-md">{review.content}</Typography>
 				<Stack direction="row" spacing={1} sx={{ alignItems: "center", flexWrap: "wrap" }}>
 					<Box>
 						<Typography level="body-sm" fontWeight="lg">
-							Jordan Sheehan, Oct 11 2024
+							{review.user.first_name} {review.user.last_name},{" "}
+							{new Date(review.created_at)
+								.toLocaleDateString("en-US", {
+									month: "short",
+									day: "numeric",
+									year: "numeric",
+								})
+								.replaceAll(",", "")}
 						</Typography>
 					</Box>
 					<Stack direction="row" sx={{ alignItems: "center" }} spacing={1}>
 						<Typography>&bull;</Typography>
 						<Typography level="body-sm" fontWeight="lg">
-							Freshman
+							{review.user.year}
+							{["First", "Second", "Third", "Fourth", "Fifth"].includes(review.user.year) && "-year"}
 						</Typography>
 					</Stack>
-					<Stack direction="row" sx={{ alignItems: "center" }} spacing={1}>
-						<Typography>&bull;</Typography>
-						<Typography level="body-sm" fontWeight="lg">
-							Traditional Single
-						</Typography>
-					</Stack>
+
+					{/* TODO: Update this pull the room the student lived in from the review */}
+					{/* <Stack direction="row" sx={{ alignItems: "center" }} spacing={1}>
+          <Typography>&bull;</Typography>
+          <Typography level="body-sm" fontWeight="lg">
+            Traditional Single
+          </Typography>
+        </Stack> */}
 				</Stack>
 			</Stack>
 		</Card>
 	);
+};
+
+Review.propTypes = {
+	review: PropTypes.object,
 };
 
 export default Review;
