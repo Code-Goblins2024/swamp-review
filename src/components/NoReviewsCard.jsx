a466901c3a999dffe4eadb66c5124366037b4482
Jordan Sheehan <jordansheehan26@gmail.com>
2024-10-28 18:46:51 -0400

    Create component for when a dorm has no reviews

---
+++ src/components/NoReviewsCard.jsx
@@ -0,0 +1,44 @@
+import PropTypes from "prop-types";
+import { Card, Typography, Button } from "@mui/joy";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+
+const NoReviewsCard = ({ housingName }) => {
+	return (
+		<Card
+			sx={{
+				display: "flex",
+				justifyContent: "center",
+				alignItems: "center",
+				padding: "2rem",
+				flexDirection: { xs: "column", sm: "row" },
+			}}
+		>
+			<Typography sx={{ textAlign: { xs: "center", sm: "start" } }} level="body-md">
+				Be the first to write a review for {housingName}!
+			</Typography>
+      
+      {/* TODO: Link this button to open the review form */}
+			<Button
+				color="primary"
+				size="md"
+				sx={{
+					display: "flex",
+					flexDirection: "row",
+					alignItems: "center",
+					gap: "0.2rem",
+				}}
+			>
+				<Typography color="white" level="body-sm">
+					Review
+				</Typography>
+				<ArrowForwardIcon color="white" fontSize="small" />
+			</Button>
+		</Card>
+	);
+};
+
+NoReviewsCard.propTypes = {
+	housingName: PropTypes.string,
+};
+
+export default NoReviewsCard;
