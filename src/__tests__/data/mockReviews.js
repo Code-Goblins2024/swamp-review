8e231a95f03408c40e57f33bf3638ef3e3e9da6a
Jordan Sheehan <jordansheehan26@gmail.com>
2024-10-30 20:04:39 -0400

    Fix mock review/housing data

--- src/__tests__/data/mockReviews.js
+++ src/__tests__/data/mockReviews.js
@@ -5,20 +5,20 @@ export const mockReviews = [
 				category: {
 					name: "Test Category 1",
 				},
-				value: 1.1,
+				value: 1.0,
 			},
 			{
 				category: {
 					name: "Test Category 2",
 				},
-				value: 2.2,
+				value: 2.0,
 			},
 		],
 		content: "Test Content 1",
 		user: {
 			first_name: "Test Firstname 1",
 			last_name: "Test Lastname 1",
-			year: "Test Year 1",
+			year: "Other",
 		},
 		created_at: "2000-01-02 02:15:00+00",
 	},
@@ -28,20 +28,20 @@ export const mockReviews = [
 				category: {
 					name: "Test Category 3",
 				},
-				value: 3.3,
+				value: 3.0,
 			},
 			{
 				category: {
 					name: "Test Category 4",
 				},
-				value: 4.4,
+				value: 4.0,
 			},
 		],
 		content: "Test Content 2",
 		user: {
 			first_name: "Test Firstname 2",
 			last_name: "Test Lastname 2",
-			year: "Test Year 2",
+			year: "Other",
 		},
 		created_at: "2001-01-02 02:15:00+00",
 	},
