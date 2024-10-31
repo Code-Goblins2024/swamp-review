ebc0ab08d00105bfd791712d87077ec716c0deaf
Jordan Sheehan <jordansheehan26@gmail.com>
2024-10-30 20:08:21 -0400

    Filter categories for test ids

--- src/functions/categoryQueries.js
+++ src/functions/categoryQueries.js
@@ -1,7 +1,7 @@
 import supabase from "../config/supabaseClient";
 
 export const getAllCategories = async () => {
-	const { data, error } = await supabase.from("categories").select("id, name");
+	const { data, error } = await supabase.from("categories").select("id, name").gt("id", -1);
 	if (error) throw error;
 	return data;
 };
