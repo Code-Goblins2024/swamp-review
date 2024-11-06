7c798af94a9a69c767b501cca859ce883a25ef41
Jordan Sheehan <jordansheehan26@gmail.com>
2024-11-06 16:21:30 -0500

    Create func for computing whether tag counts meet threshold

---
+++ src/constants/Constants.js
@@ -0,0 +1,3 @@
+// The percentage of reviews that must have a certain tag for the housing to be marked with said tag
+// e.g. if the threshold is 0.5, at least 5 out of 10 reviews must have the "Social" tag for Broward to be marked "Social"
+export const TAG_THRESHOLD = 0.5;
