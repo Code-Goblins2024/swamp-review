89ae749457275cc67f71527dea22618eeb111ba1
Jordan Sheehan <jordansheehan26@gmail.com>
2024-11-06 16:20:04 -0500

    Query for getting tag counts for specific housing

--- src/functions/tagQueries.js
+++ src/functions/tagQueries.js
@@ -5,3 +5,9 @@ export const getAllTags = async () => {
 	if (error) throw error;
 	return data;
 };
+
+export const getTagCountsForHousing = async (housingId) => {
+	const { data, error } = await supabase.rpc("get_tag_counts_for_single_housing", { housing_id_param: housingId });
+	if (error) throw error;
+	return data;
+};
