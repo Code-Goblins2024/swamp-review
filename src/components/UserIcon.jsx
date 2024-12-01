c08f55d3893568784b41f32cd700187681b7d019
VBoudreau55 <vancebou55@gmail.com>
2024-12-01 11:38:20 -0500

    added in text color changing based on icon background for improved contrast

--- src/components/UserIcon.jsx
+++ src/components/UserIcon.jsx
@@ -1,70 +1,100 @@
 import useAuth from "../store/authStore";
 import PropTypes from "prop-types";
-import { Avatar, Tooltip } from "@mui/joy";
+import { Avatar, Tooltip, Typography } from "@mui/joy";
 import UserCard from "./UserCard";
+import tinycolor from "tinycolor2";
+import {
+  useTheme,
+  getContrastRatio,
+  decomposeColor,
+} from "@mui/material/styles";
 
-const UserIcon = ({ height, width, user, bgcolor = null, hoverable = false }) => {
-	const { session, publicUser } = useAuth();
+const UserIcon = ({
+  height,
+  width,
+  user,
+  bgcolor = null,
+  hoverable = false,
+}) => {
+  const { session, publicUser } = useAuth();
+  const theme = useTheme();
 
-	const getIconColor = () => {
-		if (bgcolor !== null) {
-			return bgcolor;
-		}
-		return user?.icon_color || publicUser?.icon_color || "neutral";
-	};
+  const getIconColor = () => {
+    if (bgcolor !== null) {
+      return bgcolor;
+    }
+    return user?.icon_color || publicUser?.icon_color || "neutral";
+  };
 
-	const getInitials = () => {
-		if (user && user.first_name && user.last_name) {
-			return user?.first_name[0] + user?.last_name[0];
-		}
-		return publicUser?.first_name[0] + publicUser?.last_name[0];
-	}
+  const getInitials = () => {
+    if (user && user.first_name && user.last_name) {
+      return user?.first_name[0] + user?.last_name[0];
+    }
+    return publicUser?.first_name[0] + publicUser?.last_name[0];
+  };
 
-	return hoverable ? (
-		<Tooltip
-			placement="top-end"
-			variant="outlined"
-			arrow
-			title={
-				<UserCard
-					user_id={user?.id}
-					isEditable={false}
-				/>
-			}
-		>
-			<Avatar
-				sx={{
-					bgcolor: getIconColor(),
-					width: width,
-					height: height,
-					fontSize: `${Math.min(width, height) * 0.5}px`,
-					fontWeight: "bold",
-					variant: "soft",
-				}}
-			>
-				{getInitials()}
-			</Avatar>
-		</Tooltip>
-	) : (
-		<Avatar
-			sx={{
-				bgcolor: getIconColor(),
-				width: width,
-				height: height,
-				fontSize: `${Math.min(width, height) * 0.5}px`,
-				fontWeight: "bold",
-				variant: "soft",
-			}}
-		>
-			{getInitials()}
-		</Avatar>
-	);
+  function getContrastColor(bgColor) {
+	const color = tinycolor(bgColor, { format: "rgb" }).toRgbString();
+	
+    return getContrastRatio(color, "#fff") > 4.5
+      ? "#fff"
+      : "#111";
+  }
+
+  return hoverable ? (
+    <Tooltip
+      placement="top-end"
+      variant="outlined"
+      arrow
+      title={<UserCard user_id={user?.id} isEditable={false} />}
+    >
+      <Avatar
+        sx={{
+          bgcolor: getIconColor(),
+          width: width,
+          height: height,
+		  variant: "soft",
+        }}
+      >
+        <Typography
+          level="body-sm"
+          sx={{
+            fontSize: `${Math.min(width, height) * 0.5}px`,
+            fontWeight: "bold",
+			color: getContrastColor(getIconColor()),
+          }}
+        >
+          {getInitials()}
+        </Typography>
+      </Avatar>
+    </Tooltip>
+  ) : (
+    <Avatar
+      sx={{
+        bgcolor: getIconColor(),
+        width: width,
+        height: height,
+        variant: "soft",
+      }}
+    >
+      <Typography
+        level="body-sm"
+        sx={{
+          fontSize: `${Math.min(width, height) * 0.5}px`,
+          fontWeight: "bold",
+		  color: getContrastColor(getIconColor()),
+        }}
+      >
+        {getInitials()}
+      </Typography>
+    </Avatar>
+  );
 };
 
 UserIcon.propTypes = {
-	height: PropTypes.number,
-	width: PropTypes.number,
-	bgcolor: PropTypes.string,
+  height: PropTypes.number,
+  width: PropTypes.number,
+  bgcolor: PropTypes.string,
 };
 
 export default UserIcon;
